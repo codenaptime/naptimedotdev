5a612587079d3d18472d25db31bd84cc8e0d1fc1
naptimedev <nathan@naptime.dev>
2020-06-28 23:11:40 -0500

    Update site title tag using react-helmet. Add react-helmet

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,7 @@ module.exports = {
       },
     },
     'gatsby-transformer-remark',
+    'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-plugin-typography`,
       options: {
--- src/components/main-layout.js
+++ src/components/main-layout.js
@@ -4,6 +4,10 @@ import Header from './header';
 export default function Layout({ children }) {
         return (
             <Fragment>
+                <Helmet>
+                    <meta charSet="utf-8" />
+                    <title>Rest for your weary web.</title>
+                </Helmet>
                 <Header />
                 <main>
                 { children }</main>
